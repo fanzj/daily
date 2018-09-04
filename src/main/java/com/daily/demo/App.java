210392cdab77fc417972e62c3b220274a544dd80
范政杰 <fanzhengjie@u51.com>
2018-09-04 22:14:57 +0800

    并行流

--- src/main/java/com/daily/demo/App.java
+++ src/main/java/com/daily/demo/App.java
@@ -1,5 +1,7 @@
 package com.daily.demo;
 
+import java.math.BigDecimal;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,9 @@ public class App
 {
     public static void main( String[] args )
     {
+        BigDecimal yield = new BigDecimal("3.5256");
+        BigDecimal a = yield.setScale(2, BigDecimal.ROUND_HALF_UP);
+        System.out.println(a);
 
     }
 }
