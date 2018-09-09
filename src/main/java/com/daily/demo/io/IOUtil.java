8e952e2b697fa6cab7ef9c296cf7759c38c19bb8
范政杰 <fanzhengjie@u51.com>
2018-09-09 09:56:14 +0800

    io字节流

--- src/main/java/com/daily/demo/io/IOUtil.java
+++ src/main/java/com/daily/demo/io/IOUtil.java
@@ -1,6 +1,11 @@
 package com.daily.demo.io;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 /**
@@ -37,6 +42,105 @@ public class IOUtil {
                 in.close();
             }
         }
+    }
+
+    public static void printHexByByteArray(String fileName) throws IOException {
+        FileInputStream in = new FileInputStream(fileName);
+        byte[] buf = new byte[8 * 1024];
+        /**
+         * 从in中批量读取字节，放入到buf这个字节数组中，
+         * 从第0个位置开始放，最多放buf.length个
+         * 返回的是读到的字节的个数
+         */
+        /*int bytes = in.read(buf, 0, buf.length);//一次性读完，说明字节数组足够大
+        int j = 1;
+        for(int i=0;i<bytes;i++){
+            System.out.print(Integer.toHexString(buf[i] & 0xff) + " ");
+            if(j++%10==0){
+                System.out.println();
+            }
+        }*/
+        int bytes = 0;
+        int j = 1;
+        while((bytes = in.read(buf, 0, buf.length))!=-1){
+            for(int i=0;i<bytes;i++){
+                System.out.print(Integer.toHexString(buf[i] & 0xff) + " ");//int 32位， byte 8位，高24位清零
+                if(j++%10==0){
+                    System.out.println();
+                }
+            }
+        }
+    }
+
+    /**
+     * 文件拷贝，字节批量
+     * @param srcFile
+     * @param destFile
+     * @throws IOException
+     */
+    public static void copyFile(File srcFile, File destFile) throws IOException{
+        if(!srcFile.exists()){
+            throw new IllegalArgumentException("文件:"+srcFile+"不存在");
+        }
+        if(!srcFile.isFile()){
+            throw new IllegalArgumentException(srcFile+"不是文件");
+        }
+        FileInputStream in = new FileInputStream(srcFile);
+        FileOutputStream out = new FileOutputStream(destFile);
+        byte[] buf = new byte[8*1024];
+        int b;
+        while((b=in.read(buf,0,buf.length))!=-1){
+            out.write(buf,0,b);
+            out.flush();//最好加上
+        }
+        in.close();
+        out.close();
+    }
+
+    /**
+     * 进行文件的拷贝，利用带缓冲的字节流
+     * @param srcFile
+     * @param destFile
+     */
+    public static void copyFileByBuffer(File srcFile, File destFile) throws IOException {
+        if(!srcFile.exists()){
+            throw new IllegalArgumentException("文件:"+srcFile+"不存在");
+        }
+        if(!srcFile.isFile()){
+            throw new IllegalArgumentException(srcFile+"不是文件");
+        }
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(srcFile));
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(destFile));
+        int c;
+        while((c=bis.read())!=-1){
+            bos.write(c);
+            bos.flush();//刷新缓冲区，否则写入不到
+        }
+        bis.close();
+        bos.close();
+    }
+
+    /**
+     * 单字节，不带缓冲，进行文件拷贝
+     * @param srcFile
+     * @param destFile
+     */
+    public static void copyFileByBuyte(File srcFile, File destFile) throws IOException {
+        if(!srcFile.exists()){
+            throw new IllegalArgumentException("文件:"+srcFile+"不存在");
+        }
+        if(!srcFile.isFile()){
+            throw new IllegalArgumentException(srcFile+"不是文件");
+        }
+        FileInputStream in = new FileInputStream(srcFile);
+        FileOutputStream out = new FileOutputStream(destFile);
+        int c;
+        while((c=in.read())!=-1){
+            out.write(c);
+            out.flush();//刷新缓冲区，否则写入不到
+        }
+        in.close();
+        out.close();
     }
 
 }
