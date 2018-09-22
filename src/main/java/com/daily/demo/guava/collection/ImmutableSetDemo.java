30bba690821254acc18cda8fdd5a034daf0520d7
范政杰 <fanzhengjie@u51.com>
2018-09-22 16:45:45 +0800

    不可变集合

---
+++ src/main/java/com/daily/demo/guava/collection/ImmutableSetDemo.java
@@ -0,0 +1,25 @@
+package com.daily.demo.guava.collection;
+
+import com.google.common.collect.ImmutableMultiset;
+import com.google.common.collect.ImmutableSet;
+
+/**
+ * @author fanzhengjie
+ * @create 2018/9/19 下午8:28
+ * @description 不可变集合
+ */
+public class ImmutableSetDemo {
+
+    public static final ImmutableSet<String> COLOR_NAMES = ImmutableSet.of(
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "purple");
+
+    public static void main(String[] args) {
+
+    }
+
+}
